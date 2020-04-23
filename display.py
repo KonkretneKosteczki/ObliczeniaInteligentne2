b93c4ed283bc254b543b30dd59c08232da7367ca
Krzysztof Krzeszewski <krzysztofadamkrzeszewski@gmail.com>
2020-04-23 14:25:10 +0200

    initial commit

---
+++ display.py
@@ -0,0 +1,29 @@
+import torch
+from matplotlib import pylab as plt
+
+fig, ax = plt.subplots()
+img_batch = 0
+
+
+def display_results(mnist_test, X_test, prediction):
+    val, idx = torch.max(prediction, dim=1)
+
+    def draw():
+        for i in torch.arange(0, 12):
+            plt.subplot(4, 4, i + 1)
+            plt.imshow(X_test[img_batch * 12 + i][0])
+            plt.title('This image contains: {0:>2} '.format(idx[i].item()))
+            plt.xticks([]), plt.yticks([])
+            plt.plt.subplots_adjust()
+        plt.draw()
+
+    def press(event):
+        global img_batch
+        if event.key == 'right':
+            img_batch += 1
+        if event.key == 'left':
+            img_batch -= 1
+        draw()
+
+    fig.canvas.mpl_connect('key_press_event', press)
+    plt.show()
