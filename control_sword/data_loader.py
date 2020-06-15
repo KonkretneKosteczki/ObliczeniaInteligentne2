231893cefd2f5c7866b1ce1fab4a4f368b2dd5bc
Krzysztof Krzeszewski <krzysztof.krzeszewski@tdsoft.pl>
2020-06-15 16:47:46 +0200

    attempt at jaccard

--- control_sword/data_loader.py
+++ control_sword/data_loader.py
@@ -8,22 +8,25 @@ from PIL import Image
 from torch import Tensor
 from typing import Callable
 
+crop_size = [224, 224]  # appropriate dimensions for AlexNet
+
 
 def transform(pic: Image, offset: [int, int]) -> Tensor:
     def expand_data(*args):
         return transforms.Lambda(lambda x: x.expand(3, -1, -1))(*args)
 
+    def centered_crop(center: [int, int], height: int, width: int):
+        top = center[1] - height // 2
+        left = center[0] - width // 2
+        return crop(top, left, height, width)
+
     def crop(top: int, left: int, height: int, width: int) -> Callable[[Tensor], Tensor]:
         return lambda image: image[..., top:top + height, left:left + width]
-
-    top = offset[1] - 112
-    left = offset[0] - 112
 
     return transforms.Compose([
         transforms.ToTensor(),
         # crop centered at data form offset (center of a sword based on last visible position)
-        # crop(offset[0] - 112, offset[1] - 112, 224, 224),
-        crop(top, left, 224, 224),
+        centered_crop(offset, *crop_size),
         expand_data
     ])(pic)
 
@@ -32,14 +35,40 @@ class CustomDataSet(torch.utils.data.Dataset):
     def __init__(self, main_dir, transform):
         self.main_dir = main_dir
         self.transform = transform
+
         self.total_images = natsort.natsorted(os.listdir(main_dir))
         self.labels = read_position("C:/MOO2/control_sword/data/train/out1.txt")
+        self.offsets = list([self.calc_offset(self.get_last_pos(idx)) for idx in range(len(self.total_images))])
+        # self.sword_positions = list([self.fill_labels(self.labels[idx], idx) for idx in range(len(self.total_images))])
+
+        # remove partially obscured from training
+        for idx, pos in reversed(list(enumerate(self.labels))):
+            if "#" in pos:
+                del self.total_images[idx], self.offsets[idx], self.labels[idx]
 
     def __len__(self):
         return len(self.total_images)
 
+    def fill_labels(self, labs, idx):
+        # replaces unknown positions with last known positions
+        frame_labels = self.labels[idx - 1]
+        changed = False
+        for pos_id in range(len(labs)):
+            if labs[pos_id] == "#":
+                labs[pos_id] = frame_labels[pos_id]
+                changed = True
+        if changed:
+            labs = self.fill_labels(labs, idx - 1)
+        return labs
+
     def get_last_pos(self, idx):
-        return self.labels[idx-1] if idx != 0 else self.labels[idx]
+        return self.fill_labels(self.labels[idx - 1] if idx != 0 else self.labels[idx], idx - 1)
+
+    @staticmethod
+    def calc_cropped_position(position, offset):
+        offset_y = offset[1] - 112
+        offset_x = offset[0] - 112
+        return [position[0] - offset_x, position[1] - offset_y, position[2] - offset_x, position[3] - offset_y]
 
     @staticmethod
     def calc_offset(position: [int, int, int, int]) -> [int, int]:
@@ -47,16 +76,16 @@ class CustomDataSet(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         img_loc = os.path.join(self.main_dir, self.total_images[idx])
-        offset = self.calc_offset(self.get_last_pos(idx))
-        sword_position = self.labels[idx]
+        offset = self.offsets[idx]
+        # offset = self.calc_offset(self.get_last_pos(idx))
+        sword_position = self.fill_labels(self.labels[idx], idx)
         image = Image.open(img_loc).convert("RGB")
         tensor_image = self.transform(image, offset)
-        return {"data": tensor_image, "offset": offset, "label": sword_position}
-        # return tensor_image
+        return {"data": tensor_image, "offset": offset, "label": Tensor(self.calc_cropped_position(sword_position, offset))}
+
 
 def load_dataset(data_path):
     return CustomDataSet(data_path, transform)
-    # return torchvision.datasets.ImageFolder(root=data_path, transform=transform)
 
 
 def convert_string_position_to_int(position: str):
@@ -76,4 +105,5 @@ def read_position(file_path: str, delimiter=";"):
 
 
 def load_data(data_path: str, batch_size: int = 32):
-    return torch.utils.data.DataLoader(load_dataset(data_path), batch_size, True)
+    dataset = load_dataset(data_path)
+    return torch.utils.data.DataLoader(dataset, batch_size, True), len(dataset) // batch_size
--- control_sword/sword.py
+++ control_sword/sword.py
@@ -1,10 +1,98 @@
+from timeit import default_timer as timer
+import torchvision.models as models
+from PIL import ImageDraw, Image
+from torch.nn.modules.loss import _Loss
 from torchvision import transforms
+
 from control_sword.data_loader import load_data
+import torch
+
+batch_size = 16
+epochs = 3
+learning_rate = 0.001
+
+
+class Jaccard(_Loss):
+    def forward(self, true_bounding_boxes, own_bounding_boxes):
+        def intersection(a, b):  # returns None if rectangles don't intersect
+            dx = max(min(a[0], a[2]), min(b[0], b[2])) - min(max(a[0], a[2]), max(b[0], b[2]))
+            dy = max(min(a[1], a[3]), min(b[1], b[3])) - min(max(a[1], a[3]), max(b[1], b[3]))
+            return dx * dy if (dx <= 0) and (dy <= 0) else 0
+
+        def area(a):
+            return abs((a[0] - a[2]) * (a[1] - a[3]))
+
+        def single_jaccard(true_bounding_box, own_bounding_box):
+            inter = intersection(true_bounding_box, own_bounding_box)
+            return inter / (area(true_bounding_box) + area(own_bounding_box) - inter)
+
+        def not_so_jaccard(true_bounding_box, own_bounding_box):
+            inter = intersection(true_bounding_box, own_bounding_box)
+            return (area(true_bounding_box) + area(own_bounding_box) - 2 * inter)
+
+        return torch.mean(torch.Tensor(list(map(not_so_jaccard, true_bounding_boxes, own_bounding_boxes))))
+
+
+def train(batch_size: int, training_epochs: int, learning_rate: float, model: torch.nn.Module):
+    # criterion = Jaccard()
+    criterion = torch.nn.MSELoss()
+    print('\nTraining the Deep Learning network ...')
+
+    optimizer = torch.optim.Adam(params=filter(lambda p: p.requires_grad, model.parameters()), lr=learning_rate)
+    loader, total_batches = load_data("C:/MOO2/control_sword/parsed", batch_size)
+
+    training_start = timer()
+
+    for epoch in range(training_epochs):
+        for i, batch_data in enumerate(loader):
+            X = batch_data.get("data")
+            Y = batch_data.get("label")
+
+            optimizer.zero_grad()  # <= initialization of the gradients
+            # forward propagation
+            hypothesis = model(X)
+            cost = criterion(hypothesis, Y)  # <= compute the loss function
+            # cost.requires_grad = True
+            # Backward propagation
+            cost.backward()  # <= compute the gradient of the loss/cost function
+            optimizer.step()  # <= Update the gradients
+
+            print("Epoch= {},\t batch = {}, MSELoss = {:2.4f}".format(epoch + 1, i, cost))
+            del X, Y, hypothesis
+
+    total_time = timer() - training_start
+    print('Learning Finished! time spent = {}s, on average {}s per epoch'.format(total_time,
+                                                                                 total_time / training_epochs))
+    # return train_cost, train_accu, load_model(model, "model.pth")
+
+
+def mark_sword_on_image(img: Image, sword_coordinates: [int, int, int, int], fill=(255, 0, 255)):
+    draw = ImageDraw.Draw(img)
+    draw.line((sword_coordinates[0], sword_coordinates[1], sword_coordinates[2], sword_coordinates[3]),
+              fill=fill, width=3)
+    del draw
 
 
 if __name__ == "__main__":
-    data_loader = load_data("C:/MOO2/control_sword/parsed", 2)
-    for batch_id, batch_data in enumerate(data_loader):
-        print(batch_data)
-        transforms.ToPILImage()(batch_data.get("data")[0]).show()
-        break
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = models.alexnet(pretrained=False)
+    model.classifier[6] = torch.nn.Linear(model.classifier[6].in_features, 4)
+    model.to(device)
+    train(batch_size, epochs, learning_rate, model)
+
+    test_data_loader, total_batches = load_data("C:/MOO2/control_sword/parsed", 1)
+    # video = cv2.VideoWriter("C:/MOO2/control_sword/test.mp4", cv2.VideoWriter_fourcc(*'avc1'), 30, (224, 224))
+    for batch_id, batch_data in enumerate(test_data_loader):
+        # print(batch_id, batch_data)
+        data = batch_data.get("data")
+        labels = batch_data.get("label")
+        outputs = model(data)
+        # print(batch_data)
+        img = transforms.ToPILImage()(data[0])
+        mark_sword_on_image(img, labels[0], (0, 255, 0))
+        mark_sword_on_image(img, outputs[0], (255, 0, 255))
+        # video.write(np.array(img))
+        # video.write(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
+        img.show()
+        input("Any key to continue: ")
+    # video.release()
